366e5dee54ea87f39f7d64ef85fc4f416e4cd925
mwangiwaiganjo <88252240+mwangiwaiganjo@users.noreply.github.com>
2024-02-14 17:00:59 +0300

    new lock file

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-
-import path_provider_foundation
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-
 import app_links
 import firebase_core
 import firebase_messaging
@@ -25,5 +19,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-
 }
