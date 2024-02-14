0669d581c1d1e8edb7d9ed228d97596e4a8e09fa
kabwoyke <edwardkabwoy@gmail.com>
2024-02-14 15:02:21 +0300

    firebase fcm setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:bccf/screens/login.dart';
 import 'package:bccf/state/AuthProvider.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Permission.notification.onGrantedCallback(() {
+    print("hello perm");
+  }).request();
   await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Supabase.initialize(
     url: dotenv.env["SUPABASE_URL"]!,
     anonKey: dotenv.env["SUPABASE_ANON_KEY"]!,
@@ -44,10 +54,42 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  void initState() {
+    super.initState();
+    supabase.auth.onAuthStateChange.listen((event) async {
+      if(event.event == AuthChangeEvent.signedIn){
+        await FirebaseMessaging.instance.requestPermission();
+        await FirebaseMessaging.instance.getAPNSToken();
+        var fcmToken = await FirebaseMessaging.instance.getToken();
+
+        if(fcmToken != null){
+          await addToken(fcmToken);
+        }
+        FirebaseMessaging.instance.onTokenRefresh.listen((event) async {
+          if(fcmToken != null){
+          await addToken(fcmToken);
+          }
+          
+        });
+      }
+    });
+  }
+  Future<void> addToken(String token) async{
+    var userId =  supabase.auth.currentUser?.id;
+    if(userId != null){
+      print(userId);
+      await supabase.from("fcm_tokens").upsert({
+        'user_id' : userId,
+        'token' : token
+    });
+    }
+   
+  }
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: const LoginScreen(),
-     // This trailing comma makes auto-formatting nicer for build methods.
+     
     );
   }
 }
