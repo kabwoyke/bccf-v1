f5c99ea52d1d9200f4a923243b5da91b522c5ed5
kabwoyke <edwardkabwoy@gmail.com>
2024-02-14 18:11:54 +0300

    auth state change functionality

--- lib/main.dart
+++ lib/main.dart
@@ -95,6 +95,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: const LoginScreen());
+    return Scaffold(
+        body: StreamBuilder<AuthState>(
+            stream: supabase.auth.onAuthStateChange,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text("${snapshot.error.toString()}"),
+                );
+              }
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+
+              if (supabase.auth.currentSession == null) {
+                return LoginScreen();
+              }
+              return Homepage();
+            }));
   }
+}
 }
